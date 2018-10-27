409ffa2ccd15ea45fc0957ffeca7733119263580
Tom Dugan <thomas@aadwebb.com>
2018-10-27 13:02:41 -0500

    shows brazil customers

--- ChinookSQLQueries.sql
+++ ChinookSQLQueries.sql
@@ -1,3 +1,12 @@
+/* non_usa_customers.sql: 
+	Provide a query showing Customers (just their full names, customer ID and country) who are not in the US. */
+
 select FirstName, LastName, CustomerId, Country
 from dbo.Customer
 where Country = 'USA'
+
+/* brazil_customers.sql: Provide a query only showing the Customers from Brazil. */
+
+select *
+from dbo.Customer
+where Country = 'Brazil'
